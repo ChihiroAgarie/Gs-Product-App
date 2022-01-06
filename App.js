c433aedbd8c02286e10ee2ba06a8df750ce7e197
Chihiro Agarie <90324872+ChihiroAgarie@users.noreply.github.com>
2022-01-06 20:33:01 +0900

    20220106 初回登録→登録画面へ遷移

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
+//2022/1/6画面遷移を行うために追加
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Button, Text, View, Image, SafeAreaView, Alert } from 'react-native';
+//2022/1/6画面遷移を行うために追加
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
 
-const Start = () => {
+//スタート画面
+// const Start = () => {
+function Start() {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
@@ -11,7 +21,8 @@ const Start = () => {
         />
         <Button
           title="初回登録"
-          onPress={() => Alert.alert('初回登録へ')}
+          // onPress={() => Alert.alert('初回登録へ')}
+          onPress={() => navigation.navigate('Register')}
         />
         <Button
           title="ログイン"
@@ -22,6 +33,21 @@ const Start = () => {
   );
 }
 
+function Register() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Text>登録画面</Text>
+    </View>
+  );
+}
+
+//スタイルシート
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -36,4 +62,22 @@ const styles = StyleSheet.create({
 });
 
 
-export default Start;
+// export default Start;
+
+//2022/1/6画面遷移を行うために追加。
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Start} />
+        <Stack.Screen
+          name="Register"
+          component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
