615034442fabbfd60abd478ab654e2b97edec1f0
Chihiro Agarie <90324872+ChihiroAgarie@users.noreply.github.com>
2022-01-07 22:25:39 +0900

    220107-Register

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
-//2022/1/6画面遷移を行うために追加
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Button, Text, View, Image, SafeAreaView, Alert } from 'react-native';
-//2022/1/6画面遷移を行うために追加
+import { StyleSheet, Button, Text, View, Image, SafeAreaView, Alert, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
 
+
 //スタート画面
-// const Start = () => {
 function Start() {
+  // ナビゲーション設定
   const navigation = useNavigation();
 
   return (
@@ -21,7 +20,6 @@ function Start() {
         />
         <Button
           title="初回登録"
-          // onPress={() => Alert.alert('初回登録へ')}
           onPress={() => navigation.navigate('Register')}
         />
         <Button
@@ -33,19 +31,72 @@ function Start() {
   );
 }
 
+//登録画面
 function Register() {
+  const [text, onChangeText] = React.useState(null);
+  const [email, onChangeEmail] = React.useState(null);
+  const [password, onChangePassword] = React.useState(null);
+  // ナビゲーション設定
+  const navigation = useNavigation();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <Text>登録画面</Text>
-    </View>
+    <SafeAreaView>
+      <Text>親御様のお名前</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="山田　花子"
+      />
+      <Text>メールアドレス</Text>
+      <TextInput
+        style={styles.input}
+        onChangeEmail={onChangeEmail}
+        value={email}
+        placeholder="yamada@gmail.com"
+        keyboardType="ascii-capable"
+      />
+      <Text>パスワード</Text>
+      <TextInput
+        style={styles.input}
+        onChangePassword={onChangePassword}
+        value={password}
+        placeholder="password"
+        keyboardType="ascii-capable"
+      />
+      <Button
+        title="登録完了"
+        onPress={() => navigation.navigate('Welcome')}
+      />
+    </SafeAreaView>
   );
 }
+
+//アプリ説明画面
+function Welcome() {
+  // ナビゲーション設定
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>conico(コニコ)へようこそ！</Text>
+        <Text>conico(コニコ)は、お子様のやりたいことを見つけられるアプリです。
+        </Text>
+        <Text>親子で一緒にいろいろな経験をして、お子様の可能性を広げましょう！</Text>
+        <Text>私たちがそのお手伝いをします。</Text>
+        <Text>まずはお子様のことを教えてください。</Text>
+        <Button
+          title="次へ"
+        // onPress={() => navigation.navigate('ChildRegister')}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+//お子さん登録画面
+
 
 //スタイルシート
 const styles = StyleSheet.create({
@@ -59,14 +110,18 @@ const styles = StyleSheet.create({
     width: 150,
     height: 50,
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
 
-
-// export default Start;
-
-//2022/1/6画面遷移を行うために追加。
+//画面遷移設定
 const Stack = createStackNavigator();
 
+// 全体設定
 export default function App() {
   return (
     <NavigationContainer>
@@ -77,6 +132,12 @@ export default function App() {
         <Stack.Screen
           name="Register"
           component={Register} />
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome} />
+        {/* <Stack.Screen
+          name="Register"
+          component={ChildRegister} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
